400b565cf362b1ddc28f90a9c1b18124c38df5a5
ajay <ajaykumar290904@gmail.com>
2025-04-15 13:41:16 +0530

    Refactor About component for improved structure and styling; enhance greeting and skills display with typing effect.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,24 +1,53 @@
-import React from 'react'
+import React from "react";
+import ReactTypingEffect from "react-typing-effect";
 
 const About = () => {
   return (
-   <section
-   id='about'
-   className='py-4 px-[7vw] md:px-[20vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32'
-   >
-    <div className='flex flex-col-reverse md:flex-row justify-between items-center'>
-      {/* left side */ }
-      
-      <div className='md:w-1/2 text-center md:text-left mt-8 md:mt-0'></div>
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* left side */}
 
-      {/* Greeting */ }
-      <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold text-white'>
-        Hi, I am
-      </h1>
-    </div>
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* Greeting */}
+          <h1 className="text-2xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
 
-   </section>
-  )
-}
+          {/* Name */}
 
-export default About
+          <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-4 leading-tight">
+            Ajay kumar
+          </h2>
+          {/* Skills heading with typing effect */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold  mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I am a</span>
+            <ReactTypingEffect
+              text={[" Coder", " Fullstack Developer"]}
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => {
+                <span className="text-[#8245ec]">{cursor}</span>;
+              }}
+            ></ReactTypingEffect>
+          </h3>
+          {/* about me paragraph */}
+          <p className="text-base sm:text-lg md:text-lg text-gray-400 mt-10 mb-8 leading-relaxed">
+            I'm Ajay Kumar, a passionate and detail-oriented Full Stack
+            Developer with a strong foundation in both frontend and backend
+            technologies. On the frontend, I specialize in building responsive,
+            user-friendly interfaces using React.js, Tailwind CSS, and
+            JavaScript. On the backend, I work with Express, and MongoDB to
+            create robust and scalable APIs and applications.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
