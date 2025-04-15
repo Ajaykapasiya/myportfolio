6d02a74bedc9d89605f6b90872be77e82f608f6e
ajay <ajaykumar290904@gmail.com>
2025-04-15 12:35:38 +0530

    Add BlurBlob component with animation and position props; update Tailwind CSS configuration for blob animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",  // very important to scan your component files
+    "./src/**/*.{js,jsx,ts,tsx}", // very important to scan your component files
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blob: {
+          '0%': {transform: 'scale(1)'},
+          '33%': {transform: 'scale(1.2)'},
+          '66%': {transform: 'scale(0.8)'},
+          '100%': {transform: 'scale(1)'},
+        },
+      },
+      animation:{
+        blob: 'blob 10s infinite'
+      }
+    },
   },
   plugins: [],
-}
+};
