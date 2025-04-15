379f7671848a2d043c2ff9fcf5d12e10fdc8b20e
ajay <ajaykumar290904@gmail.com>
2025-04-15 13:04:28 +0530

    Refactor About component to use section element for semantic structure; update styling for improved layout and responsiveness.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
+   <section
+   id='about'
+   className='py-4 px-[7vw] md:px-[20vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32'
+   >
+    <div className='flex flex-col-reverse md:flex-row justify-between items-center'>
+      {/* left side */ }
       
+      <div className='md:w-1/2 text-center md:text-left mt-8 md:mt-0'></div>
+
+      {/* Greeting */ }
+      <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold text-white'>
+        Hi, I am
+      </h1>
     </div>
+
+   </section>
   )
 }
 
