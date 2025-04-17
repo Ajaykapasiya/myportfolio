d6ddfec88cff9d2af048ac281200ea295bb27ec9
ajay <ajaykumar290904@gmail.com>
2025-04-17 12:19:45 +0530

    Refactor Experience and Skills components for improved layout and styling; add background gradient to Skills section and enhance Skills categories display.

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { experiences } from '../../constants'
 
 const Experience = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    
+  <section
+  id='experience'
+  className='py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient '
+  >
+    {/* Section title */}
+   <div className='text-center mb-16'>
+    
+    
+     <h2 className='text-4xl font-bold text-white'>Experience</h2>
+   </div>
+  </section>  
+);
 }
 
 export default Experience
