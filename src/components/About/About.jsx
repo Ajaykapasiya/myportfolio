d3392891079c8dd362651a525008b78af11e4372
ajay <ajaykumar290904@gmail.com>
2025-07-19 16:11:56 +0530

    fix: update resume link in About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1cDjITkk4-bfpMcvxS6N8WWnHUeA_NYVb/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1NvNMBIydx8WftTdEVDrdoEXkmO6zNWK4/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
